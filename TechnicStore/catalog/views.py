d1d2ce011985ea6644c008b112bf1e13cc82b7e2
MaksMBO <maksbredyk@gmail.com>
2023-03-01 19:16:42 +0200

    functionality added to catalog

--- TechnicStore/catalog/views.py
+++ TechnicStore/catalog/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.http import HttpResponse
 
-# Create your views here.
+    
+class Catalog(ListView):
+    
+    def get(self, request):
+         return HttpResponse("Catalog")
+
+
+class AboutProduct(DetailView):
+    
+    pk_url_kwarg = 'id'
+    
+    def get(self, request, *args, **kwargs):
+         return HttpResponse("AboutProduct")
+
+         
+class ProductComparison(ListView):
+    
+    def get(self, request):
+         return HttpResponse("ProductComparison")
+
+class FilterProducts(ListView):
+    
+    def get(self, request):
+         return HttpResponse("FilterProducts")
+    
+class FilterProductsJson(ListView):
+
+    def get(self, request, *args, **kwargs):
+        return HttpResponse("FilterProductsJson")
