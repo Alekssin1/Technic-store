bffa08646a1bf282adea81e581d4b2bc503cf89b
MaksMBO <maksbredyk@gmail.com>
2023-03-31 14:18:35 +0300

    add filters

--- TechnicStore/catalog/views.py
+++ TechnicStore/catalog/views.py
@@ -38,15 +38,52 @@ class ProductComparison(ListView):
     def get(self, request):
          return HttpResponse("ProductComparison")
 
-class FilterProducts(ListView):
     
-    def get(self, request):
-         return HttpResponse("FilterProducts")
+class FilterProductsJson(CatalogMixin, ListView):
+    def get_queryset(self):
+        all_brands = [i.brand for i in ProductsBrand.objects.all()]
+        if not self.request.GET.get("brand", False):
+            brand =  Q(brand__brand__in=all_brands)
+        else:
+            brand = Q(brand__brand__in=self.request.GET.getlist('brand'))
+      
+        if self.request.GET.getlist('price_up'):
+            sort = 'price'
+            sort_text = 'за зростанням ціни'
+        elif self.request.GET.getlist('price_down'):
+            sort = '-price'
+            sort_text = 'за зменшенням ціни'
+        else:
+            sort = 'id'
+            sort_text = 'за популярністю'
+            
+        query = Products.objects.filter(
+            brand &
+            Q(price__gte=self.request.GET.get('price_start'), price__lte=self.request.GET.get('price_end'))
+            ).prefetch_related('img').values('title', 'price', 'img__img', 'id').distinct().order_by(sort)
     
-class FilterProductsJson(ListView):
+        return query, sort_text
 
     def get(self, request, *args, **kwargs):
-        return HttpResponse("FilterProductsJson")
+        queryset, sort_text = self.get_queryset()
+        temp = []
+        temp.clear()
+
+        queryset = list(queryset)
+        my_len = len(queryset)    
+        i = 0
+        while i < my_len:
+            if queryset[i]['title'] not in temp:
+                temp.append(queryset[i]['title'])
+                i += 1
+            else:
+                del queryset[i]
+                my_len -= 1
+                
+        context= {'products': queryset,
+                  'sort': sort_text}
+       
+        return render(request, 'catalog/partials/catalog__product.html', context=context)
     
 class SearchProductsJson(CatalogMixin, ListView):
     def post(self, request, *args, **kwargs):
