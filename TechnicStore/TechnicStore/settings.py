1a22f6673b5138838a1d047b010ead2152565ac0
MaksMBO <maksbredyk@gmail.com>
2023-04-08 14:20:10 +0300

    fix

--- TechnicStore/TechnicStore/settings.py
+++ TechnicStore/TechnicStore/settings.py
@@ -86,6 +86,13 @@ DATABASES = {
         'PASSWORD': 'root',
         'HOST': '127.0.0.1',
         'PORT': '3306',
+        'OPTIONS': {
+        'charset': 'utf8mb4',
+        'init_command': 'SET character_set_connection=utf8mb4;'
+                        'SET collation_connection=utf8mb4_unicode_ci;'
+                        "SET NAMES 'utf8mb4';"
+                        "SET CHARACTER SET utf8mb4;"
+    },
     }
 }
 
